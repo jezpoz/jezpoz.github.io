b645683618bc968a5e0bf19b7b5e9068492fc15c
jezpoz <jesfor94@gmail.com>
2019-05-27 19:33:42 +0900

    google analytics... cuz why not

---
+++ precache-manifest.bd2d963373a9550bbb85bb834846ea2c.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b237a18db94bc624d40ab8e79aeced38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b593f59d3562664a07d",
+    "url": "main.js"
+  }
+]);
